5f00b89f627d3875e0d3c5a522b0240cbc76d1a4
Daminian667 <daminian111@gmail.com>
2022-10-26 17:00:16 +0300

    hardcode frontend pc version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import img4 from './images/quality.svg';
 import img5 from './images/service.svg';
 import img6 from './images/team.svg';
 import img7 from './images/equip.svg';
-
+import src1 from './images/Rectangle 85.jpg';
+import src2 from './images/Rectangle 88.jpg';
+import src3 from './images/Rectangle 86.jpg';
+import src4 from './images/Rectangle 89.jpg';
+import src5 from './images/Rectangle 117.jpg';
+import src6 from './images/Rectangle 118.jpg';
+import src7 from './images/Rectangle 119.jpg';
+import src8 from './images/Rectangle 121.jpg';
+import src9 from './images/Rectangle 120.jpg';
+import src10 from './images/Rectangle 122.jpg';
+import src11 from './images/Rectangle 123.jpg';
+import logo from './images/logo__footer.svg';
 function App() {
   return (
     <div className="App">
@@ -85,18 +96,105 @@ function App() {
           <div className='form'>
             <form className='form__container container'>
               <p className='form__title'>Ответьте на несколько вопросов и мы перезвоним вам с предложением</p>
-              <input id="name" type="text" name="name"></input>
-              <input id="email" type="email" name="email"></input>
-              <input id="phone" type="phone" name="phone"></input>
-              <input id="adress" type="text" name="adress"></input>
-              <input id="data" type="data" name="data"></input>
-              <input id="time" type="time" name="time"></input>
-              <input id="event" type="text" name="event"></input>
-              <input id="number_of_gests" type="text" name="number_of_gests"></input>
-              <button className='form__button' type='submit'>отправить</button>
+              <input id="name" type="text" name="name" placeholder='ваше имя'></input>
+              <input id="email" type="email" name="email" placeholder='ваш емейл'></input>
+              <input id="phone" type="tel" name="phone" placeholder='телефон'></input>
+              <input id="adress" type="text" name="adress" placeholder='адрес проведения'></input>
+              <input id="data" type="date" name="data" placeholder='дата мероприятия'></input>
+              <input id="time" type="time" name="time" placeholder='тайминг мероприятия'></input>
+              <input id="event" type="text" name="event" placeholder='формат мероприятия'></input>
+              <input id="number_of_gests" type="number" name="number_of_gests" placeholder='колличество гостей'></input>
+              <button className='form__button' type='submit' onClick={(e) => { e.preventDefault(); console.log("click")}}>отправить</button>
             </form>
           </div>
+          <div className='galery'>
+            <p className='galery__title container'>Галерея блюд</p>
+            <div className='galery__grid'>
+              <img src={src1} className='grid__item a'></img>
+              <img src={src2} className='grid__item b'></img>
+              <img src={src3} className='grid__item c'></img>
+              <img src={src4} className='grid__item d'></img>
+              <img src={src5} className='grid__item e'></img>
+              <img src={src6} className='grid__item f'></img>
+              <img src={src8} className='grid__item h'></img>
+              <img src={src9} className='grid__item i'></img>
+              <img src={src10} className='grid__item j'></img>
+              <img src={src11} className='grid__item k'></img>
+              <img src={src7} className='grid__item g'></img>
+            </div>
+          </div>
+          <div className='review container'>
+              <div className='review__bg'>
+
+              <h3 className='review__title'>Отзывы о компании Chesnokoff</h3>
+              <div className='review__container'>
+                <p className='review__text'> 
+                  Отзывы о нашей компании настоящие и написаны нашими дорогими клиентами. 
+                  Вы так же можете оставить отзыв о нас!
+                </p>
+                <button className='review__button'>НАПИСАТЬ ОТЗЫВ</button>
+              </div>
+            </div>
+          </div>
+          <div className='feedback container'>
+            <p className='feedback__text'>
+              Хотите что бы ваше мероприятие прошло безупречно
+              и все ваши гости были сыты и довольны?
+              </p>
+            <p className='feedback__text'>
+              Доверьте эту задачу Chesnokoff и мы гарантируем успех!
+              Оставьте свои контакты и мы вам перезвоним с пердложением!
+            </p>
+            <div className='feedback__form'>
+              <input className='feedback__input' id="name" type="text" name="name" placeholder='ваше имя'></input>
+              <input className='feedback__input' id="email" type="email" name="email" placeholder='ваш емейл'></input>
+              <input className='feedback__input' id="phone" type="tel" name="phone" placeholder='телефон'></input>
+              <button className='feedback__button'>ОТПРАВИТЬ</button>
+            </div>
+          </div>
       </main>
+      <footer className='footer'>
+        <div className='footer__container container'>
+          <img className='footer__logo' src={logo}></img>
+          <nav className='footer__nav'>
+            <ul className='footer__list'>
+              <li className='footer__item'>
+                <a className='footer__link'>СВАДЬБА</a>
+              </li>
+              <li className='footer__item'>
+                <a className='footer__link'>ВЫСТАВКА</a>
+              </li>
+              <li className='footer__item'>
+                <a className='footer__link'>НОВЫЙ ГОД</a>
+              </li>
+              <li className='footer__item'>
+                <a className='footer__link'>ДОСТАВКА</a>
+              </li>
+              <li className='footer__item'>
+                <a className='footer__link'>КЕЙТЕРИНГ</a>
+              </li>
+            </ul>
+          </nav>
+          <nav className='footer__nav'>
+            <ul className='footer__list'>
+              <li className='footer__item'>
+                <a className='footer__link'>КОМАНДА</a>
+              </li>
+              <li className='footer__item'>
+                <a className='footer__link'>ПАРТНЕРАМ</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='footer__contacts'>
+            <p className='footer__adress'>
+              Бульвар Космонавтов, д. 9
+            </p>
+            <p className='footer__phone'>
+              +7 999 888 77 66
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
